c4debc61d21365e97761eb9a215ec9f3bd575c81
juangalbea <juangalbea@gmail.com>
2019-12-22 23:47:53 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// index.js
+
+let paragraph = document.getElementById('paragraph');
+
+console.log(paragraph); // <== <p id="paragraph">What is your name?</p>
+// In the JavaScript above, to select the paragraph element, we will use the document.getElementById() method. This method returns the selected p node.
+
+// We can also select an element by its className. But be careful with this method. It will always return an array of DOM nodes, no matter if it finds one or more elements in the code.
+
+let links = document.getElementsByClassName('link');
+
+console.log(links); // <== HTMLCollection [a#google-link.link, google-link: a#google-link.link]
+                    // 0: a#google-link.link
+                    // length: 1
+                    // google-link: a#google-link.link
+                    // __proto__: HTMLCollection
+
+let link = document.querySelector('a'); 
+
+link.setAttribute('href', 'http://www.google.com');                    
